27cf04a2bccd0a085a288db47da6b3cb9141ef66
arkin0x <arkinox@pm.me>
2024-06-01 23:01:58 -0500

    deleted old hyperjumps

--- src/deleteHyperjump.ts
+++ src/deleteHyperjump.ts
@@ -1,3 +1,42 @@
 // use this script to delete hyperjump objects from the relay
 
 // I should delete all hyperjumps before timestamp 1717299665 
+
+import dotenv from 'dotenv';
+import { getPublicKey, getEventHash, getSignature, SimplePool } from 'nostr-tools';
+import type { Event, UnsignedEvent } from 'nostr-tools';
+
+dotenv.config();
+
+console.log(getPublicKey(process.env.PRIVATE_KEY as string))
+
+function deleteEvent(event: Event) {
+  console.log('deleting', event.id)
+  const pubkey = getPublicKey(process.env.PRIVATE_KEY as string)
+  const deletion = {
+    kind: 5,
+    pubkey,
+    created_at: Math.floor(Date.now() / 1000),
+    tags: [
+      ["e", event.id],
+    ],
+    content: `Deleting test hyperjump`,
+  } as UnsignedEvent;
+  const id = getEventHash(deletion);
+  const signedEvent = { ...deletion, id } as Event;
+  const sig = getSignature(deletion, process.env.PRIVATE_KEY as string);
+  signedEvent.sig = sig
+  pool.publish([relayUrl], signedEvent)
+}
+
+const relayUrl = 'wss://cyberspace.nostr1.com';
+
+const pool = new SimplePool();
+
+const oldHyperjumps = pool.sub([relayUrl], [{kinds: [321], until: 1717299665}]);
+
+oldHyperjumps.on('event', deleteEvent)
+
+oldHyperjumps.on('eose', () => {
+  console.log('deletion complete!')
+})
