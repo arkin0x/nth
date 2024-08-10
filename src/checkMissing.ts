10805bf93d3d1c322a9315f47b2296b32ff897a8
arkin0x <arkinox@pm.me>
2024-08-09 23:13:19 -0500

    got all missing blocks

--- src/checkMissing.ts
+++ src/checkMissing.ts
@@ -6,6 +6,8 @@ import WebSocket from "ws";
 if (!global.WebSocket) {
   global.WebSocket = WebSocket as any;
 }
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pb = new PocketBase("http://127.0.0.1:8090");
 
